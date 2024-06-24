59d141fba94fbb07364d77834fc2b277bdd2339a
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-06-24 12:49:45 +0000

    initial commit

---
+++ gateway/src/test/java/com/redolf/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redolf.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
