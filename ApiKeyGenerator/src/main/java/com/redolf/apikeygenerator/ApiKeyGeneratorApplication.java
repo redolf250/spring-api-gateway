1d0337d5c893387f7cbb8978bb427faa5bee1540
redolf250 <55606159+redolf250@users.noreply.github.com>
2024-06-26 17:54:07 +0000

    api key generator service added

---
+++ ApiKeyGenerator/src/main/java/com/redolf/apikeygenerator/ApiKeyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.redolf.apikeygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiKeyGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiKeyGeneratorApplication.class, args);
+    }
+
+}
---
+++ ApiKeyGenerator/src/test/java/com/redolf/apikeygenerator/ApiKeyGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redolf.apikeygenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiKeyGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
